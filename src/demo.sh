e0bd1daa1d182c5f58a80bfb6bb5e608530b4ae1
Peter Frandsen <pfrandsen@gmail.com>
2018-01-14 23:22:13 +0100

    hide cursor; fix issue (yes got broken pipe when recording) - replace with printf

--- src/demo.sh
+++ src/demo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+setterm --cursor off
 # Intro
 
 clear
 sleep 1
-yes '' | head -n5
+printf '\n%.0s' {1..5}
 toilet -f ivrit 'Code Rocks!' | boxes -d cat -a hc -p h8 | ./goslow -s 40000
 sleep 3; clear;
 
@@ -14,10 +15,10 @@ sleep 3; clear;
 # show source code statistics
 sleep 1
 clear
-yes '' | head -n5
+printf '\n%.0s' {1..5}
 echo -e "Source Code Statistics`cat stats.txt | awk '{print}' ORS='\\t\\t\\n' | head -c -3`" | boxes -d ian_jones | ./goslow -s 25000 | lolcat
 sleep 4
-yes '' | head -n50 | ./goslow -s 25000
+printf '\n%.0s' {1..50} | ./goslow -s 25000
 clear
 
 # outtro - team presentation
@@ -29,8 +30,9 @@ sleep 1
 figlet -c "Thanx for watching" | ./goslow -s 40000 | lolcat
 sleep 5
 # script note
-yes '' | head -n50 | ./goslow -s 25000
+printf '\n%.0s' {1..50} | ./goslow -s 25000
 echo -e "\n\t\tCode demo scripts by\n\t\tPeter Frandsen\n\t\tpfrandsen@gmail.com" | boxes -d santa | ./goslow -s 25000
 sleep 2
-yes '' | head -n50 | ./goslow -s 25000
-sleep 7
+printf '\n%.0s' {1..50} | ./goslow -s 25000
+sleep 1
+setterm --cursor off
